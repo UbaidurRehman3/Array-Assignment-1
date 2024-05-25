68a3f1b7c6f3887dc62089dac8f65f2c0dc43591
UbaidurRehman3 <uk43559@gmail.com>
2024-05-25 17:44:14 +0500

    Add files via upload

---
+++ Q1.ts
@@ -0,0 +1,5 @@
+// Qus no 1
+ //create an array named fruits that contains the following 
+//string elements: "apple", "banana", "mango", " Orange"
+let fruits : string[] = ["apple", "banna", "mango", "orange"];
+console.log(fruits);
