af43ae184081bbb613bc876325961b6596981665
UbaidurRehman3 <uk43559@gmail.com>
2024-05-25 17:50:37 +0500

    Add files via upload

---
+++ Q9.ts
@@ -0,0 +1,5 @@
+//Qus no 9 
+//remove 2 elements from the fruit array string from index 1 using the method
+ let fruits : string [] = ["apple", "banna", "mango", "orange"];
+ fruits.splice(1,2);
+ console.log(fruits);
