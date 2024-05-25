2cd6ebe0e660cfdcefc2e5b9647592f963f798f3
UbaidurRehman3 <uk43559@gmail.com>
2024-05-25 17:46:06 +0500

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,5 @@
+//Qus no 4 
+//chang the second element of the numbers array to 25.
+var numbers = [10, 20, 30, 40];
+numbers[1] = 25;
+console.log(numbers);
---
+++ Q4.ts
@@ -0,0 +1,5 @@
+//Qus no 4 
+//chang the second element of the numbers array to 25.
+let numbers : number[]= [10,20,30,40];
+    numbers[1] = 25;
+    console.log(numbers);
