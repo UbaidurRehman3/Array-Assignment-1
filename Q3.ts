10cd2bcc9b3a8cc8b0cc0fe7b364240deb40277e
UbaidurRehman3 <uk43559@gmail.com>
2024-05-25 17:45:27 +0500

    Add files via upload

---
+++ Q3.ts
@@ -0,0 +1,4 @@
+// Qus no 3 
+//access the third element of the fruits array and assign it to a variable named thirthfruit.
+let fruits : string [] = ["apple", "banana", "mango", "orange"];
+console.log(fruits[2]);
