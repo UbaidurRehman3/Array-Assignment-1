68f01ca9e314b720c2ce220176563c75352343ab
UbaidurRehman3 <uk43559@gmail.com>
2024-05-25 17:47:23 +0500

    Add files via upload

---
+++ Q6.ts
@@ -0,0 +1,5 @@
+// Qus no 6 
+//remove the last element from the fruits array using the method and assign it to a variable named lastFruits
+let fruits : string [] = ["apple", "banana", "mango", "orange"];
+fruits.pop();
+console.log(fruits);
