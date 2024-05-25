7e57b1c6f0cfc3f10d15d048ee6987c3fb4f1458
UbaidurRehman3 <uk43559@gmail.com>
2024-05-25 17:50:01 +0500

    Add files via upload

---
+++ Q8.ts
@@ -0,0 +1,5 @@
+//Qus no 8 
+//Add the element "kiwi" to the beginning of the fruits array using the method.
+let fruits : string [] = ["apple", "banana", "mango", "orange"];
+fruits.unshift("kiwi");
+console.log(fruits);
