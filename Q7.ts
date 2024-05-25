d7e50139907dfe140d65b39e0a9f221ef3963a4c
UbaidurRehman3 <uk43559@gmail.com>
2024-05-25 17:48:03 +0500

    Add files via upload

---
+++ Q7.ts
@@ -0,0 +1,5 @@
+//Qus no 7
+//remove the first element from the fruits array using the method and assign it to a variable named firstFruit.
+let fruits : string [] = ["apple", "banana", "mango", "orange"];
+fruits.shift();
+console.log(fruits);
